a57c4c8f14fc5b6ff045b589a31a7ed54ff239e7
Dericrandomusername <107108479+Dericrandomusername@users.noreply.github.com>
2023-05-08 16:24:17 +0800

    Add files via upload

---
+++ water.js
@@ -0,0 +1,30 @@
+function setup(){
+    canvas=createCanvas(640,420)
+    canvas.center()
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+    document.getElementById("status").innerHTML="status:detecting objects"
+}
+status="";
+objects=[];
+function modelLoaded(){
+    console.log("model Loaded");
+    status="true";
+    objectDetector.detect(img,gotResults)
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+
+    }
+    console.log(results);
+    objects=results
+}
+function preload(){
+    img=loadImage("water.jpg");
+}
+function draw(){
+    image(img,0,0,640,420);
+   if(status!=""){
+
+   }
+}
